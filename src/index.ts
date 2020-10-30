8819943f0077bde07fb4be4883319ce605ccc2d3
Andres Rodriguez <andrese.rodriguezh@gmail.com>
2020-10-30 11:07:04 -0500

    copy over the logic from changelog-git

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,49 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop')
+import {
+  NewChangesetWithCommit,
+  VersionType,
+  ChangelogFunctions,
+  ModCompWithPackage,
+} from '@changesets/types'
+
+const getReleaseLine = async (
+  changeset: NewChangesetWithCommit,
+  _type: VersionType
+) => {
+  const [firstLine, ...futureLines] = changeset.summary
+    .split('\n')
+    .map(l => l.trimRight())
+
+  let returnVal = `- ${
+    changeset.commit ? `${changeset.commit}: ` : ''
+  }${firstLine}`
+
+  if (futureLines.length > 0) {
+    returnVal += `\n${futureLines.map(l => `  ${l}`).join('\n')}`
   }
-  return a + b
+
+  return returnVal
 }
+
+const getDependencyReleaseLine = async (
+  changesets: NewChangesetWithCommit[],
+  dependenciesUpdated: ModCompWithPackage[]
+) => {
+  if (dependenciesUpdated.length === 0) return ''
+
+  const changesetLinks = changesets.map(
+    changeset => `- Updated dependencies [${changeset.commit}]`
+  )
+
+  const updatedDepenenciesList = dependenciesUpdated.map(
+    dependency => `  - ${dependency.name}@${dependency.newVersion}`
+  )
+
+  return [...changesetLinks, ...updatedDepenenciesList].join('\n')
+}
+
+const defaultChangelogFunctions: ChangelogFunctions = {
+  getReleaseLine,
+  getDependencyReleaseLine,
+}
+
+export default defaultChangelogFunctions
