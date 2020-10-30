461196cbe0108213e052a9c3a38db18310b1c924
Andres Rodriguez <andrese.rodriguezh@gmail.com>
2020-10-30 15:49:52 -0500

    mimic changesets github changelog generator

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,71 @@
 import {ChangelogFunctions} from '@changesets/types'
 import {config} from 'dotenv'
+import {getBitbucketInfo} from './getBitbucketInfo'
 
 config()
 
+export type ChangesetChangelogBitbucketOptions = {
+  repo: string
+}
+
 const changelogFunctions: ChangelogFunctions = {
-  getReleaseLine: async (changeset, _type) => {
+  getReleaseLine: async (changeset, _type, options) => {
+    if (!options || !options.repo) {
+      throw new Error(
+        'Please provide a repo to this changelog generator like this:\n"changelog": ["@changesets/changelog-github", { "repo": "org/repo" }]'
+      )
+    }
     const [firstLine, ...futureLines] = changeset.summary
       .split('\n')
       .map(l => l.trimRight())
 
-    let returnVal = `- ${
-      changeset.commit ? `${changeset.commit}: ` : ''
-    }${firstLine}`
-
-    if (futureLines.length > 0) {
-      returnVal += `\n${futureLines.map(l => `  ${l}`).join('\n')}`
+    if (changeset.commit) {
+      let {links} = await getBitbucketInfo({
+        repo: options.repo,
+        commit: changeset.commit,
+      })
+      return `\n\n- ${links.commit}${
+        links.pull === null ? '' : ` ${links.pull}`
+      }${
+        links.user === null ? '' : ` Thanks ${links.user}!`
+      } - ${firstLine}\n${futureLines.map(l => `  ${l}`).join('\n')}`
+    } else {
+      return `\n\n- ${firstLine}\n${futureLines.map(l => `  ${l}`).join('\n')}`
     }
-
-    return returnVal
   },
-  getDependencyReleaseLine: async (changesets, dependenciesUpdated) => {
+  getDependencyReleaseLine: async (
+    changesets,
+    dependenciesUpdated,
+    options
+  ) => {
+    if (!options.repo) {
+      throw new Error(
+        'Please provide a repo to this changelog generator like this:\n"changelog": ["changesets-changelog-bitbucket", { "repo": "workspace/repoSlug" }]'
+      )
+    }
     if (dependenciesUpdated.length === 0) return ''
-
-    const changesetLinks = changesets.map(
-      changeset => `- Updated dependencies [${changeset.commit}]`
+    const changesetLink = `- Updated dependencies [${(
+      await Promise.all(
+        changesets.map(async cs => {
+          if (cs.commit) {
+            let {links} = await getBitbucketInfo({
+              repo: options.repo,
+              commit: cs.commit,
+            })
+            return links.commit
+          }
+          return null
+        })
+      )
     )
+      .filter(Boolean)
+      .join(', ')}]:`
 
     const updatedDepenenciesList = dependenciesUpdated.map(
       dependency => `  - ${dependency.name}@${dependency.newVersion}`
     )
 
-    return [...changesetLinks, ...updatedDepenenciesList].join('\n')
+    return [changesetLink, ...updatedDepenenciesList].join('\n')
   },
 }
 
